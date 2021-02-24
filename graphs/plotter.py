f3436fc97183cb43f78a77dec2ba3c5ec31b2810
bekiekB1 <56314761+bekiekB1@users.noreply.github.com>
2021-02-24 10:36:59 -0600

    added learning curve to graphs->plotter

--- graphs/plotter.py
+++ graphs/plotter.py
@@ -13,3 +13,13 @@ def plot_loss_acc(name,score):
     plt.ylabel(name.split(sep=' ')[1])
     plt.plot(score)
     plt.savefig("graphs/"+name+".png")
+
+def lerning_curve(train_loss, dev_loss):
+    plt.figure(figsize=(10, 6))
+    plt.plot( train_loss,"r-+", linewidth=3, label="Training loss")
+    plt.plot(dev_loss, "b-", linewidth=2, label="Dev loss")
+    plt.legend(loc="best", fontsize=14)   
+    plt.xlabel("Epochs", fontsize=14) 
+    plt.ylabel("Loss", fontsize=14) 
+    plt.title("Learning Curve")
+    plt.savefig("graphs/"+name+".png")
